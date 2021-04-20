d62fd74664902e531acda0ec02d6b309d24ec22a
4boysdontcry <hgy02166@gmail.com>
2021-04-20 12:05:22 +0900

    cookie 적용

--- js/index.js
+++ js/index.js
@@ -1,33 +1,41 @@
 /*************** 글로벌 설정 *****************/
-init()
+init()    //페이지를 열자마자 실행될 함수
 
 /*************** 사용자 함수 *****************/
-function init (){
-  $('.header-wrapper').find('.notice-content').hide()
-  $('.header-wrapper').find('.bt-hide').hide()
+function init() {
+	if($.cookie('hideNotice') === 'Y') onCloseNotice()
+	else {
+		$('.header-wrapper').find('.notice-content').hide()
+		$('.header-wrapper').find('.bt-hide').hide()
+	}
 }
 
 
 /*************** 이벤트 등록 *****************/
-$('.header-wrapper .bt-show').click(onNoticeShow)
-$('.header-wrapper .bt-hide').click(onNoticeHide)
-$('.header-wrapper .bt-close').click(onNoticeClose)
-
+$('.header-wrapper .bt-show').click(onShowNotice)
+$('.header-wrapper .bt-hide').click(onHideNotice)
+$('.header-wrapper .bt-close').click(onCloseNotice)
+$('.header-wrapper .bt-today').click(onHideTodayNotice)
 
 
 /*************** 이벤트 콜백 *****************/
-function onNoticeShow(){
+function onShowNotice(){
   $('.header-wrapper').find('.bt-show').hide()
   $('.header-wrapper').find('.bt-hide').show()
   $('.header-wrapper').find('.notice-content').show()
 }
 
-function onNoticeHide(){
+function onHideNotice(){
   $('.header-wrapper').find('.bt-show').show()
   $('.header-wrapper').find('.bt-hide').hide()
   $('.header-wrapper').find('.notice-content').hide()
 }
 
-function onNoticeClose (){
+function onCloseNotice (){
   $('.header-wrapper').find('.notice-wrapper').hide()
+}
+
+function onHideTodayNotice(){
+  $.Cookie('hideNotice', 'Y', { expires: 1, path: '/' })
+  onCloseNotice()
 }
