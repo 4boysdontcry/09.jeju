015a774d3f3897388e95cfbdb60c446d80354a85
4boysdontcry <hgy02166@gmail.com>
2021-04-23 13:05:33 +0900

    pager 구성중

--- js/index.js
+++ js/index.js
@@ -10,50 +10,78 @@
 $(function() {
 
 	/*************** 글로벌 설정 *****************/
-	var $wrapper = $('.main-wrapper')
-	var $slide = $('.main-wrapper .slide')
-	var video = $('.main-wrapper .video')[0]
-	var len = $slide.length
-	var lastIdx = len - 1
-	var depth = 2
-	var idx = 0
-	var gap = 3000
-	var speed = 500
-	init()
+var $wrapper = $('.main-wrapper')
+var $slide = $('.main-wrapper .slide')
+var $pagerSlide = $('.main-wrapper .pager-slide')
+var video = $('.main-wrapper .video')[0]
+var len = $slide.length
+var lastIdx = len - 1
+var depth = 2
+var idx = 0
+var gap = 3000
+var speed = 500
+init()
 	
 	
 	/*************** 사용자 함수 *****************/
-	function init() {
-		$slide.eq(idx).css('z-index', depth++)
+function init() {
+    $slide.eq(idx).css('z-index', depth++)
     $slide.eq(idx).addClass('active')
-		onAni()
-	}
+    for(var i=0; i<len; i++){
+        $('<i class="pager"></i>').appendTo($pagerSlide).click(onPagerClick).addClass((idx === i)?'active':'')
+    }
+	ani()
+}
 
 
 	/*************** 이벤트 등록 *****************/
-	video.addEventListener('loadeddata', onLoadedVideo)
-	video.addEventListener('ended', onPlay)
+video.addEventListener('loadeddata', onLoadedVideo)
+video.addEventListener('ended', onPlay)
+$('.bt-video').click(onModalVideo)
+$('.modal-video').find('.bt-close').click(onModalVideoClose)
+$('.cookie-wrapper').find('.bt-close').click(onCookieClose)
 
-  
+
+
 	/*************** 이벤트 콜백 *****************/
-	function onLoadedVideo() {
-		if(video.readyState >= 2) {
-			video.playbackRate = 4.0
-		}
+function onPagerClick(){
+    idx = $(this).index()
+    $pagerSlide.find('.pager').removeClass('active')
+    $(this).addClass('active')
+    onPlay('pager')
+}
+
+function onLoadedVideo() {
+	if(video.readyState >= 2) {
+		video.playbackRate = 4.0
 	}
+}
+
+function onModalVideo(){
+    $('.modal-video').show()
+}
+
+function onModalVideoClose(){
+    $('.modal-video').hide()
+}
+
+function onCookieClose(){
+    $('.cookie-wrapper').hide()
+}
 	
-	function onAni() {
+function ani() {
     $(this).addClass('active')
 		video.currentTime = 0
 		if($slide.eq(idx).hasClass('is-video')) video.play()
 		else setTimeout(onPlay, gap)
-	}
+}
 
-	function onPlay() {
-		idx = (idx == lastIdx) ? 0 : idx + 1
-		$slide.eq(idx).css({'z-index': depth++, 'left': '100%'})
+function onPlay(state) {
+	if(state !== 'pager') idx = (idx == lastIdx) ? 0 : idx + 1
+	$slide.eq(idx).css({'z-index': depth++, 'left': '100%'})
     $slide.removeClass('active')
-		$slide.eq(idx).stop().animate({'left': 0}, speed, onAni)
-	}
+	$slide.eq(idx).stop().animate({'left': 0}, speed, ani)
+}
+
 
 })
