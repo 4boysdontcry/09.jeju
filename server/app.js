ecb4260dc04f6eaaa6aac93d9b4fac2c92535b86
4boysdontcry <hgy02166@gmail.com>
2021-05-03 10:24:43 +0900

    서버 만드는중

---
+++ server/app.js
@@ -0,0 +1,17 @@
+
+var express = require('express')
+var app = express()
+
+
+app.listen(3000,function(){
+        console.log('http://127.0.0.1:3000')
+})
+
+app.get('/', function(req, res){
+    res.send('<h1>서버가 시작되었습니다.</h1>')
+})
+
+// npm init -y
+// npm i express
+// app.js 만들고
+// node app
