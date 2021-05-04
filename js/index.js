1324b62b5b0a6f304ece925cd92a0b9eacb5814f
4boysdontcry <hgy02166@gmail.com>
2021-05-04 13:48:04 +0900

    room 만드는중

--- js/index.js
+++ js/index.js
@@ -16,7 +16,6 @@ $(function () {
 	var slick = {
 		autoplay: true,
 		autoplaySpeed: 2000,
-		pauseOnDotsHover: true,
 		infinite: true,
 		touchThreshold: 10,
 		arrows: false,
@@ -266,6 +265,10 @@ $(function () {
 		function onGetData(r) {
 			room = r.room.slice();
 			options.slidesToShow = 2;
+			options.dots = false;
+			options.responsive.pop();		// pop 배열의 맨 마지막 요소를 빼낸다.(지워준다)
+			options.responsive[0].breakpoint = 992;
+			options.responsive[0].settings.slidesToShow = 1;
 			$slick.slick(options);
 			makeSlickButton($slick, $btPrev, $btNext);
 			$(window).trigger('resize');
@@ -403,23 +406,33 @@ $(function () {
 		emailjs.init('user_pwPZEzPsmfPrNGtfDQt7X');
 	}
 
-	/* *********Global Function******** */
-	function makeSlickButton($slick, $prev, $next){
-		$prev.click(function() { $slick.slick('slickPrev') });
-		$next.click(function() { $slick.slick('slickNext') });
-	}
+}
+/* *********Global Function******** */
+function makeSlickButton($slick, $prev, $next){
+	$prev.click(function() {
+		$slick.slick('slickPrev') 
+	});
+	$next.click(function() { 
+		$slick.slick('slickNext')
+	});
+	$slick.find('.slick-dots').on('mouseenter', function() {
+    $slick.slick('slickPause');
+});
+	$slick.find('.slick-dots').on('mouseleave', function() {
+    $slick.slick('slickPlay');
+});
+}
 
-	function onResize(e) {
-		$('.ratio-wrap').each(function(i) {
-			var ratio = $(this).data('ratio') // data-ratio
-			var width = $(this).innerWidth();
-			var height = width * Number(ratio);
-			$(this).innerHeight(height);
-		})
-	}
-	
-	$(window).resize(onResize).trigger('resize');
-	}
+function onResize(e) {
+	$('.ratio-wrap').each(function(i) {
+		var ratio = $(this).data('ratio') // data-ratio
+		var width = $(this).innerWidth();
+		var height = width * Number(ratio);
+		$(this).innerHeight(height);
+	})
+}
+
+$(window).resize(onResize).trigger('resize');
 
 
 
